1c5588396be3eaf65308a9224fa19a8d2696f4a3
math1761 <mathieulevi@gmail.com>
2017-12-25 14:14:39 +0100

    Finished exercice

--- index.js
+++ index.js
@@ -2,26 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const search = require('./modules/search');
-const databaseConfig = require('./config');
+const db = require('./config');
 const path = require('path');
-const {body, validationResult} = require('express-validator/check');
-const {sanitizeBody} = require('express-validator/filter');
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname));
-databaseConfig.connectDatabase();
+db.connectDatabase();
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.post('/', (req, res) => {
-
-    /*const cache = new Images({
-        tag: req.body.tag
-    });*/
 
     search.searchFlickr(req.body.tag, res);
 
--- modules/search.js
+++ modules/search.js
@@ -2,6 +2,10 @@ const Flickr = require('flickr-sdk');
 const path = require('path');
 const img_path = path.join(__dirname, '/public/images');
 flickr = new Flickr("10f0db070e0069eeb6fc3b423ab58510");
+const db = require('../config')
+const url = 'mongodb://localhost:27017';
+const dbName = 'flickr-iesa';
+const MongoClient = require('mongodb').MongoClient;
 
 module.exports = {
 
@@ -14,15 +18,25 @@ module.exports = {
         flickr.photos.search({
             text: tag
         }).then(function (res) {
-            for (i = 0; i < 30; i++) {
-                result.write("<div class='grid-item'>" +
-                    "<img src='https://farm" + res.body.photos.photo[i].farm + ".staticflickr.com/" +
-                    res.body.photos.photo[i].server + "/" + res.body.photos.photo[i].id + "_" +
-                    res.body.photos.photo[i].secret + ".jpg" + "'/>" + "</div>")
+            if (tag != null) {
+                for (i = 0; i < 30; i++) {
+                    result.write("<div class='grid-item'>" +
+                        "<img src='https://farm" + res.body.photos.photo[i].farm + ".staticflickr.com/" +
+                        res.body.photos.photo[i].server + "/" + res.body.photos.photo[i].id + "_" +
+                        res.body.photos.photo[i].secret + ".jpg" + "'/>" + "</div>")
+                }
+                result.end();
             }
-            result.end();
         }).catch(function (err) {
-            console.error(err);
+            MongoClient.connect(url, function (err, client) {
+                if (err) throw err;
+                const db = client.db(dbName);
+                db.collection("Tags").find({}).toArray(function (err, resultdb) {
+                    if (err) throw err;
+                    result.write("<img class='img_error' src='" + resultdb[0].tag.image1 + "'/>")
+                    result.end();
+                });
+            });
         });
     }
 
